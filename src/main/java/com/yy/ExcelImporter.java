5353d85de46b72011e4eb085b97233c06ee54373
kk <k@qq.com>
2018-03-22 22:59:34 +0800

    导入到数据库的处理类

---
+++ src/main/java/com/yy/ExcelImporter.java
@@ -0,0 +1,11 @@
+package com.yy;
+
+/**
+ * Created by storm on 2018/3/22.
+ * 导入到数据库的类
+ */
+public class ExcelImporter {
+    public static void main(String[] args) {
+        System.out.printf("hello");
+    }
+}
